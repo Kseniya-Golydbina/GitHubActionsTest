92f1745c1a25e043d5a35c1778e96d60c3cb1e2d
Kseniya-Golydbina <kseniya.golydbina@yandex.ru>
2022-12-01 19:30:18 +0300

    init

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,23 +2,13 @@
 
 public class Calculator {
     public static void main(String[] args) {
-        /*Scanner scanner=new Scanner(System.in);
-        System.out.print("Введите два числа: ");
-        int x=scanner.nextInt();
-        int y=scanner.nextInt();
-        System.out.print("Выберите математическую операцию: " + "(+, -, *, /)");
-        int k=scanner.nextInt();
-        System.out.println("Результат математической операции: ");*/
-
     }
     public int add(int x, int y){
         return x+y;
     }
     public int diff(int x, int y){
-
         return x-y;
     }
-
     public int increase(int x, int y){
         return x*y;
     }
